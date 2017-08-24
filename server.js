3d567ef4dac8ec6251fbe8b55401b82cbaf1d7f0
Johan Behrens <johan.behrens@liberty.co.za>
2017-08-24 16:39:55 +0200

    added cors

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ var Parser = require('node-dbf');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 // log to console
 app.use(morgan('dev'));
 
