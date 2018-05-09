3935ff6dad7980488c41b33705d622371af11e15
Johan Behrens <johan.behrens@liberty.co.za>
2018-05-09 19:01:38 +0200

    added time o2

--- server.js
+++ server.js
@@ -434,7 +434,7 @@ apiRoutes.post('/heartBeat/setIp', function(req, res) {
             return res.send({success: false, msg: err});
         }
 
-        socketServer.setValue(req.body.identifier, '10', done);
+        socketServer.setValue(req.body.identifier, Buffer.from('10', 'hex'), done);
         function done(err) {
             if (err) {
                 console.log(err);
