d6e7299f6242bc25598a8b0e012be2b5d7604ccc
Johan Behrens <johan.behrens@liberty.co.za>
2018-05-16 22:09:35 +0200

    added flash

--- server.js
+++ server.js
@@ -434,7 +434,7 @@ apiRoutes.post('/heartBeat/setIp', function(req, res) {
             return res.send({success: false, msg: err});
         }
 
-        socketServer.setFlash(req.body.identifier, Buffer.from('0', 'hex'), done);
+        socketServer.setFlash(req.body.identifier, Buffer.from('01', 'hex'), done);
         function done(err) {
             if (err) {
                 console.log(err);
