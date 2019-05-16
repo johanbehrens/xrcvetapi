0a864fbe1579295eda36d7b74219de5000d205bd
Johan Behrens <johan.behrens@liberty.co.za>
2019-05-16 08:49:39 +0200

    added api

---
+++ api.js
@@ -0,0 +1,70 @@
+ /*
+    "bcrypt": "^0.8.5",
+    "body-parser": "~1.9.2",
+    "epson-thermal-printer": "^1.0.0",
+    "express": "~4.9.8",
+    "js-crc": "^0.2.0",
+    "jwt-simple": "^0.3.1",
+    "mongoose": "~4.2.4",
+    "morgan": "~1.5.0",
+    "net": "^1.0.2",
+    "node-dbf": "^0.2.1",
+    "node-rest-client": "^3.1.0",
+    "node-thermal-printer": "^1.1.1",
+    "passport": "^0.3.0",
+    "passport-jwt": "^1.2.1",
+    "raw-socket": "^1.5.2",
+    "read-dir-files": "^0.1.1",
+    "serialport": "^6.0.5"*/
+
+const express = require('express');
+const initDb = require("./db").initDb;
+
+var bodyParser = require('body-parser');
+var events = require('./routes/events');
+var horses = require('./routes/horses');
+var riders = require('./routes/riders');
+var rides = require('./routes/rides');
+var images = require('./routes/images');
+var location = require('./routes/location');
+var friends = require('./routes/friends');
+var payments = require('./routes/payments');
+var user = require('./routes/user');
+var track = require('./routes/track');
+var results = require('./routes/results');
+var passport	= require('passport');
+const fileUpload = require('express-fileupload');
+var cors = require('cors')
+const app = express();
+app.use(cors());
+const port = 80;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(fileUpload());
+
+require('./config/passport')(passport);
+var routes = require('./routes/router');
+
+app.use('/', routes);
+app.use('/events', passport.authenticate('jwt', { session: false}), events);
+app.use('/horses', passport.authenticate('jwt', { session: false}), horses);
+app.use('/riders', passport.authenticate('jwt', { session: false}), riders);
+app.use('/rides', passport.authenticate('jwt', { session: false}), rides);
+app.use('/location', passport.authenticate('jwt', { session: false}), location);
+app.use('/friends', passport.authenticate('jwt', { session: false}), friends);
+app.use('/payments', passport.authenticate('jwt', { session: false}), payments);
+app.use('/user', passport.authenticate('jwt', { session: false}), user);
+app.use('/track', passport.authenticate('jwt', { session: false}), track);
+app.use('/results', results);
+app.use('/images', images);
+
+initDb({}, function (err) {
+    app.listen(port, function (err) {
+        if (err) {
+            throw err; //
+        }
+        console.log("API Up and running on port " + port);
+    });
+});
