f6549211dcbebce45b358a3a77a7321b369cf858
Johan Behrens <johan.behrens@liberty.co.za>
2020-02-04 13:37:01 +0200

    added Jarryd

--- addons/elmicom.js
+++ addons/elmicom.js
@@ -81,10 +81,12 @@ function AddMeter(req, response) {
     if(req.body && (req.body.eth == 'down' || req.body.eth == 'up')) {
         sendPush(new ObjectID('5cb967737ff245517c5a7165'),req.params.meterId, 'Ethernet', req.body.eth);
         sendPush(new ObjectID('5cbb0ed05d1ddc6311e0c588'),req.params.meterId, 'Ethernet', req.body.eth);
+        sendPush(new ObjectID('5e34f845b883f109838a5af6'),req.params.meterId, 'Ethernet', req.body.eth);
     }
     if(req.body && (req.body.status == 'down' || req.body.status == 'up')) {
         sendPush(new ObjectID('5cb967737ff245517c5a7165'),req.params.meterId, 'Status', req.body.status);
         sendPush(new ObjectID('5cbb0ed05d1ddc6311e0c588'),req.params.meterId, 'Status', req.body.status);
+        sendPush(new ObjectID('5e34f845b883f109838a5af6'),req.params.meterId, 'Status', req.body.status);
     }
 
     function sendPush(id, meterId, type, status) {
