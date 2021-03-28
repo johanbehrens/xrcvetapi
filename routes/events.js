b3a1abe9c94ae09bb6b67c95dc05bfbd4a329e3f
Johan Behrens <johan.behrens@liberty.co.za>
2021-03-28 14:47:11 +0200

    fix small issues

--- routes/events.js
+++ routes/events.js
@@ -567,14 +567,22 @@ function GetEvents(req, res) {
 
         list.forEach(event => {
             if (event) {
-                let deventDate = new Date(event.start);
+                let deventDate = new Date();
+                let dendDate = new Date();
+
+                if (event.type == 'PERSONAL') {
+                    deventDate = event.date;
+                    dendDate = event.date;
+                }
+                else {
+                    dendDate = new Date(event.end);
+                    deventDate = new Date(event.start);
+                }
+
                 let eventDate = deventDate.toISOString().split('T')[0];
 
                 let dtoday = new Date();
                 let today = dtoday.toISOString().split('T')[0];
-
-                
-                let dendDate = new Date(event.end);
 
                 if (event.type != 'PERSONAL' && eventDate != dendDate.toISOString().split('T')[0] && deventDate <= dtoday && dtoday <= dendDate.toISOString().split('T')[0]) {
                     event._active = true;
