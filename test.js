c6038fad1cd21da5aa0968f921cb0a90ce20ad7c
Johan Behrens <johan.behrens@liberty.co.za>
2021-04-01 20:42:10 +0200

    fix small issues

--- test.js
+++ test.js
@@ -7,7 +7,25 @@ initDb({}, function (err) {
     enqueueJob({
         jobName: 'workflow',
         data: {
-            workflowId: '60660ba12edaab303f14597c'
+            workflowId: '5dee231260099605f85280f0'
+        },
+    }, function (err, arg) {
+        console.log(err)
+        console.log(arg)
+    })
+    enqueueJob({
+        jobName: 'workflow',
+        data: {
+            workflowId: '603cdd7436d30305340b876a'
+        },
+    }, function (err, arg) {
+        console.log(err)
+        console.log(arg)
+    })
+    enqueueJob({
+        jobName: 'workflow',
+        data: {
+            workflowId: '603ce08036d30305340b8773'
         },
     }, function (err, arg) {
         console.log(err)
